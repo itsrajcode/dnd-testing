274966422a68dbca59278588ed6a3750754ee123
Rajan Prajapati <rajanprajapati0526@gmail.com>
2025-01-30 00:04:19 +0530

    Feat: Canvas building

---
+++ components/Canvas.jsx
@@ -0,0 +1,14 @@
+"use client";
+import { useScreenSize } from "@/app/provider";
+import React from "react";
+
+function Canvas() {
+  const { screenSize, setScreenSize } = useScreenSize();
+  return (
+    <div className="mt-20 flex justify-center">
+      <div className={`bg-white p-6 w-full max-w-2xl ${screenSize === "desktop" ? "max-w-2xl" : "max-w-md"}`}></div>
+    </div>
+  );
+}
+
+export default Canvas;
