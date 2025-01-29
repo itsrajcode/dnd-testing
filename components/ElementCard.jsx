60b5dafd616268f1e9b394b55d32e720eca6dad0
Rajan Prajapati <rajanprajapati0526@gmail.com>
2025-01-29 23:32:05 +0530

    Fix: fixed Element sidebar

---
+++ components/ElementCard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ElementCard({ item, index }) {
+  return (
+    <div key={index} className="mb-4">
+      <div className="flex flex-col border border-dashed p-2 rounded-xl items-center gap-2 group cursor-grab hover:shadow-md hover:border-blue-500">
+        <item.icon className="w-5 h-5" />
+        <span>{item.label}</span>
+      </div>
+    </div>
+  );
+}
+
+export default ElementCard;
--- components/ElementSidebar.jsx
+++ components/ElementSidebar.jsx
@@ -1,18 +1,22 @@
 import Layout from "@/Data/Layout";
 import React from "react";
+import ElementCard from "./ElementCard";
+import Element from "@/Data/Element";
 
 function ElementSidebar() {
   return (
-    <div className="p-4">
+    <div className="p-4 h-screen shadow-sm">
       <h2 className="text-lg font-bold">Layouts</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
         {Layout.map((item, index) => (
-          <div key={index} className="mb-4">
-            <div className="flex flex-col border border-dashed p-2 rounded-xl items-center gap-2 group cursor-grab hover:shadow-md hover:border-blue-500">
-              <item.icon className="w-6 h-6" />
-              <span>{item.label}</span>
-            </div>
-          </div>
+          <ElementCard item={item} index={index} />
+        ))}
+      </div>
+
+      <h2 className="text-lg font-bold">Elements</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
+        {Element.map((item, index) => (
+          <ElementCard item={item} index={index} />
         ))}
       </div>
     </div>
