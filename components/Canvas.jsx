00ae74a050f66a3b7e7b5cf1be283cdf3b8c8cd0
Rajan Prajapati <rajanprajapati0526@gmail.com>
2025-01-30 21:51:22 +0530

    feat: Drag and drop

--- components/Canvas.jsx
+++ components/Canvas.jsx
@@ -1,14 +1,33 @@
 "use client";
 import { useScreenSize } from "@/app/provider";
 import React from "react";
+import { useDragDropElementLayout } from "@/app/provider";
 
 function Canvas() {
   const { screenSize, setScreenSize } = useScreenSize();
+  const { dragElementLayout, setDragElementLayout } = useDragDropElementLayout();
+  const onDragOver = (e) => {
+    e.preventDefault();
+    console.log("dragging");
+  };
+  const onDropHandler = (e) => {
+    e.preventDefault();
+    console.log(dragElementLayout);
+  };
   return (
     <div className="mt-20 flex justify-center">
-      <div className={`bg-white p-6 w-full max-w-2xl ${screenSize === "desktop" ? "max-w-2xl" : "max-w-md"}`}></div>
+      <div
+        className={`bg-white p-6 w-full max-w-2xl ${
+          screenSize === "desktop" ? "max-w-2xl" : "max-w-md"
+        }`}
+        onDragOver={onDragOver}
+        onDrop={onDropHandler}
+      >
+
+      </div>
     </div>
   );
 }
 
 export default Canvas;
+
--- components/ElementSidebar.jsx
+++ components/ElementSidebar.jsx
@@ -1,26 +1,44 @@
+"use client";
 import Layout from "@/Data/Layout";
 import React from "react";
 import ElementCard from "./ElementCard";
 import Element from "@/Data/Element";
+import { useDragDropElementLayout } from "@/app/provider";
 
 function ElementSidebar() {
+  const { dragElementLayout, setDragElementLayout } = useDragDropElementLayout();
+  const onDragLayoutStart = (layout) => {
+    setDragElementLayout({
+      dragLayout: {
+        ...layout,
+        id: Date.now()
+      }
+    });
+  };
   return (
     <div className="p-4 h-screen shadow-sm">
       <h2 className="text-lg font-bold">Layouts</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
         {Layout.map((item, index) => (
-          <ElementCard item={item} index={index} />
+          <div
+            key={index}
+            draggable
+            onDragStart={(e) => onDragLayoutStart(item)}
+          >
+            <ElementCard item={item} />
+          </div>
         ))}
       </div>
 
       <h2 className="text-lg font-bold">Elements</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
         {Element.map((item, index) => (
-          <ElementCard item={item} index={index} />
+          <div key={index} draggable>
+            <ElementCard item={item} />
+          </div>
         ))}
       </div>
     </div>
   );
 }
-
 export default ElementSidebar;
