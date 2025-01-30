e4986cba9eccc57848168525b676d816037cfac4
Rajan Prajapati <rajanprajapati0526@gmail.com>
2025-01-30 21:49:10 +0530

    Fix: fixed drag and provider

--- app/provider.js
+++ app/provider.js
@@ -1,14 +1,19 @@
-"use client"
-import React, { createContext, useContext, useState } from 'react';
+"use client";
+import { DragDropLayoutElement } from "@/context/DragDropLayoutElement";
+import React, { createContext, useContext, useState } from "react";
 
 const ScreenSizeContext = createContext();
 
 export function ScreenSizeProvider({ children }) {
-  const [screenSize, setScreenSize] = useState('desktop');
+  const [screenSize, setScreenSize] = useState("desktop");
+
+  const [dragElementLayout, setDragElementLayout] = useState();
 
   return (
     <ScreenSizeContext.Provider value={{ screenSize, setScreenSize }}>
-      {children}
+      <DragDropLayoutElement.Provider value={{dragElementLayout, setDragElementLayout}}>
+        {children}
+      </DragDropLayoutElement.Provider>
     </ScreenSizeContext.Provider>
   );
 }
@@ -16,7 +21,15 @@ export function ScreenSizeProvider({ children }) {
 export function useScreenSize() {
   const context = useContext(ScreenSizeContext);
   if (context === undefined) {
-    throw new Error('useScreenSize must be used within a ScreenSizeProvider');
+    throw new Error("useScreenSize must be used within a ScreenSizeProvider");
+  }
+  return context;
+}
+
+export function useDragDropElementLayout() {
+  const context = useContext(DragDropLayoutElement);
+  if (context === undefined) {
+    throw new Error("useDragDropElementLayout must be used within a DragDropLayoutElementProvider");
   }
   return context;
 }
