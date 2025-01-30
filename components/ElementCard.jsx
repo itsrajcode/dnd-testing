6d3f72fbfcbd52c031d305790a491092475e206c
Rajan Prajapati <rajanprajapati0526@gmail.com>
2025-01-30 21:50:50 +0530

    Refactor: unnessary

--- components/ElementCard.jsx
+++ components/ElementCard.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-function ElementCard({ item, index }) {
+function ElementCard({ item}) {
   return (
-    <div key={index} className="mb-4">
+    <div className="mb-4">
       <div className="flex flex-col border border-dashed p-2 rounded-xl items-center gap-2 group cursor-grab hover:shadow-md hover:border-blue-500">
         <item.icon className="w-5 h-5" />
         <span>{item.label}</span>
