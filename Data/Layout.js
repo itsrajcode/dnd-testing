dc8e494f3974053d5ab3ad6e380edc703d63c5bb
Rajan Prajapati <rajanprajapati0526@gmail.com>
2025-02-01 18:23:52 +0530

    Fix: type fix

--- Data/Layout.js
+++ Data/Layout.js
@@ -11,19 +11,19 @@ export default [
     },
     {
         label: "column 2",
-        type: "column-2",
+        type: "column",
         numOfColumns: 2,
         icon: Columns2
     },
     {
         label: "column 3",
-        type: "column-3",
+        type: "column",
         numOfColumns: 3,
         icon: Columns3
     },
     {
         label: "column 4",
-        type: "column-4",
+        type: "column",
         numOfColumns: 4,
         icon: Columns4
     },
